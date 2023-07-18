265435dc4b7d2f525ee5e18bf9b9bb1f94127a97
SLIME <purpleblueslime@gmail.com>
2023-07-18 12:55:57 +0530

    Feet(s): nyanpantsu_v0.0.01  💦

---
+++ src/components/img.js
@@ -0,0 +1,31 @@
+'use client'
+
+import {useState, useEffect} from 'react'
+import styles from 'nyan/styles/img.module.scss'
+
+export default function component({className, color, height, id, img}) {
+
+  const [lazy, $lazy] = useState(true) // lazy ;-;
+
+  // complete stuff owO (if onload don't happen)
+  useEffect(() => {
+    if (!lazy) $lazy(true)
+    const {complete} = app.querySelector(`#💦${img}`)
+    if (!complete) return
+    $lazy(false)
+  }, [img])
+
+  return (
+    <div className={className} id={id}>
+      <div className={styles.imgWrap}>
+        <img alt={'img'} className={styles.img} id={`💦${img}`} loading={'lazy'} onLoad={() => $lazy(false)} src={`/imgs/${img}`} />
+        {lazy ? <div className={styles.lazy} style={{minHeight: height}}>
+          <div className={styles.barWrap}>
+            <div className={styles.bar} style={{background: color}} />
+          </div>
+        </div>:<></>}
+      </div>
+    </div>
+  )
+
+}
